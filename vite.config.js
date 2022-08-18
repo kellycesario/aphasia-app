7211a495681fbb4436a871255816966ff30eec99
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-08-18 09:38:26 -0300

    Alias for icons and components

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@icons": resolve(__dirname, "src", "components", "icons"),
+      "@components": resolve(__dirname, "src", "components"),
+    },
+  },
+});
