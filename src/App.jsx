5780aa19fcacfac0f9a61ce11c66af3da115ef2f
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-08-18 09:40:42 -0300

    Global css and Card component imported into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import "./styles/global.scss";
+import Card from "./components/Card/Card";
 function App() {
-
   return (
     <div className="App">
-    
+      <Card title="bla bla bla" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
