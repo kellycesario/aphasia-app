2125f5142225f515c31edd0530e74445c1581431
kellycesario <kellycesario@gmail.com>
2022-09-22 18:17:43 -0300

    Passing icon size as props

--- src/components/CardsWrapper/CardsWrapper.jsx
+++ src/components/CardsWrapper/CardsWrapper.jsx
@@ -17,21 +17,21 @@ import {
 export default function CardsWrapper() {
   return (
     <div className="cardsWrapper">
-      <Card size="big" icon={<InclineBed />} title="Incline Bed" />
-      <Card size="big" icon={<LayDown />} title="Lay Down" />
-      <Card size="big" icon={<SitUp />} title="Sit Up" />
+      <Card size="big" icon={<InclineBed size="70"/>} title="Incline Bed" />
+      <Card size="big" icon={<LayDown size="70"/>} title="Lay Down" />
+      <Card size="big" icon={<SitUp size="70"/>} title="Sit Up" />
 
-      <Card size="big" icon={<NeedMedicine />} title="Need Medicine" />
-      <Card size="big" icon={<NurseOrDoctor />} title="Nurse or Doctor" />
-      <Card size="big" icon={<Pain />} title="In Pain" />
+      <Card size="big" icon={<NeedMedicine size="70"/>} title="Need Medicine" />
+      <Card size="big" icon={<NurseOrDoctor size="70"/>} title="Nurse or Doctor" />
+      <Card size="big" icon={<Pain size="70"/>} title="In Pain" />
 
-      <Card size="big" icon={<FoodDrink />} title="Food/Drink" />
-      <Card size="big" icon={<Tv />} title="TV" />
-      <Card size="big" icon={<Bath />} title="Bath" />
+      <Card size="big" icon={<FoodDrink size="70"/>} title="Food/Drink" />
+      <Card size="big" icon={<Tv size="70"/>} title="TV" />
+      <Card size="big" icon={<Bath size="70"/>} title="Bath" />
 
-      <Card size="big" icon={<ChangeBed />} title="Change Bed" />
-      <Card size="big" icon={<Emotions />} title="Emotions" />
-      <Card size="big" title="Yes / No" />
+      <Card size="big" icon={<ChangeBed size="70"/>} title="Change Bed" />
+      <Card size="big" icon={<Emotions size="70"/>} title="Emotions" />
+      <Card size="bigger" title="Yes / No" />
     </div>
   );
 }
