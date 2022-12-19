721ca04874a3a9393b89ac9438af043e1dfcd083
kellycesario <kellycesario@gmail.com>
2022-12-19 14:58:14 -0300

    Change in the color/fill of the icons

--- src/components/atoms/Icons/BodyParts/Eye/index.jsx
+++ src/components/atoms/Icons/BodyParts/Eye/index.jsx
@@ -10,7 +10,7 @@ export default function Eye({ size }) {
     >
       <g
         transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-        fill="#22202A"
+        fill="#000000"
         stroke="none"
       >
         <path
