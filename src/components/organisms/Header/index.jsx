e62491927d8813167e1aac259bbfe2e8969710b6
lucas-kenji_valtech <lucas.kenji@valtech.com>
2022-12-20 08:12:19 -0300

    create function to open and close menu

--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const onToggleHandler = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="mainHeader">
       <div className="mainHeader__container">
         <h2 className="mainHeader__logo">Aphasia App</h2>
         <nav className="mainHeader__nav">
           <ul
-            className="mainHeader__list mainHeader__list--active"
-            aria-expanded="false"
+            className={`mainHeader__list ${
+              isMenuOpen && 'mainHeader__list--active'
+            }`}
+            aria-expanded={isMenuOpen}
           >
             <li className="mainHeader__item">Home</li>
             <li className="mainHeader__item">About Us</li>
             <li className="mainHeader__item">Contact</li>
             <li className="mainHeader__item">Settings</li>
           </ul>
-          <button className="mainHeader__toggle">
+          <button className="mainHeader__toggle" onClick={onToggleHandler}>
             <img src="/assets/img/Toggle.svg" alt="toggle menu" />
           </button>
         </nav>
