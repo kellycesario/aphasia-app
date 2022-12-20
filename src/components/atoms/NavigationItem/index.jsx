eafa8b096e1669a8b06f64e6caf8e784fb010dcc
lucas-kenji_valtech <lucas.kenji@valtech.com>
2022-12-20 08:32:10 -0300

    separete header in components

---
+++ src/components/atoms/NavigationItem/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.scss';
+
+const NavigationItem = ({ title }) => {
+  return <li className="mainHeader__item">{title}</li>;
+};
+
+export default NavigationItem;
