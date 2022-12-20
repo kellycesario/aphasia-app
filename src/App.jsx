7d9878880aae4eba3c154bcf3e11c05351243f84
kellycesario <kellycesario@gmail.com>
2022-12-20 10:52:49 -0300

    Add global variables (CSS)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./styles/global.scss";
+import "./styles/variables.css";
 import LoginPage from "./pages/Login"
 import CardsWrapper from './components/organisms/CardsWrapper'
 function App() {
