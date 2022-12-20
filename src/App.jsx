632ee89db6b5e9d58d38b9dcacf2bf97984a4ed2
KenjiWorkES <lucas.kenjies@gmail.com>
2022-12-20 12:11:18 -0300

    resolve missing layout import error

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import '@styles/global.scss';
 import '@styles/variables.css';
 import LoginPage from '@pages/Login';
 import CardsWrapper from '@organisms/CardsWrapper';
+import Layout from './layout';
 function App() {
   return (
     <Layout>
